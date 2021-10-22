ae8b6a60e52603fbb0a7a3704fb47da5dabe324c
Alexey Strakh <alstrakh@microsoft.com>
2021-10-22 14:53:29 -0700

    absolute layout - move programmatically

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,41 @@
-﻿using System;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Essentials;
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
+using System.Diagnostics;
 
 namespace MauiAbsoluteLayout
 {
 	public partial class MainPage : ContentPage
 	{
-		int count = 0;
+        private Rectangle _startedBounds;
 
-		public MainPage()
+        public MainPage()
 		{
 			InitializeComponent();
+
+			var panGesture = new PanGestureRecognizer();
+			panGesture.PanUpdated += PanGesture_PanUpdated;
+			MyGrid.GestureRecognizers.Add(panGesture);
+
 		}
 
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
-			CounterLabel.Text = $"Current count: {count}";
+        private void PanGesture_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    _startedBounds = AbsoluteLayout.GetLayoutBounds(MyGrid);
+                    break;
+                case GestureStatus.Running:
+                    var newBounds = new Rectangle(_startedBounds.X + e.TotalX, _startedBounds.Y + e.TotalY, _startedBounds.Width, _startedBounds.Height);
+                    AbsoluteLayout.SetLayoutBounds(MyGrid, newBounds);
+                    break;
+                case GestureStatus.Completed:
+                    _startedBounds = Rectangle.Zero;
+                    break;
+            }
 
-			SemanticScreenReader.Announce(CounterLabel.Text);
-		}
-	}
+            Debug.WriteLine($"PanGesture_PanUpdated: GestureId={e.GestureId} | StatusType={e.StatusType} | XY={e.TotalX},{e.TotalY} | CurrentBounds={_startedBounds}");
+        }
+    }
 }
